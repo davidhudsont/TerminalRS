b91221cdb3b735a340d221595a0f6978adbea1f7
David Hudson <davidhudsont@gmail.com>
2022-06-17 18:25:41 -0700

    WIP terminal program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,225 @@
+use serialport::{DataBits, StopBits};
+
+mod xmodem;
+
+use xmodem::XModem;
+
+use std::fs::File;
+
+use std::time::Duration;
+
+use eframe::egui::{self, Response, Event, Key};
+use serialport::SerialPort;
+
 fn main() {
-    println!("Hello, world!");
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        Box::new(MyApp::default()),
+        options,
+    );
 }
+
+#[derive(Debug, PartialEq)]
+enum ComPort
+{
+    None,
+    COMPORT(String),
+}
+
+#[derive(Debug, PartialEq)]
+enum BuadRates {
+    None,
+    BAUD(i32)
+}
+
+struct MyApp {
+    name: String,
+    selected_comport: ComPort,
+    comports: Vec<ComPort>,
+    selected_buadrate: BuadRates,
+    buadrates: Vec<BuadRates>,
+    console_text: String,
+    serial_settings_flag: bool,
+    serial_port: Option<Box<dyn SerialPort>>,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "651R2/A Firmware Upgrade Application".to_owned(),
+            selected_comport: ComPort::None,
+            comports: vec![],
+            selected_buadrate: BuadRates::None,
+            buadrates: vec![BuadRates::BAUD(9600), BuadRates::BAUD(115200)],
+            console_text: "".to_owned(),
+            serial_settings_flag: false,
+            serial_port: None,
+        }
+    }
+}
+
+fn selectable_text(ui: &mut egui::Ui, mut text: &str) -> Response {
+    ui.add(egui::TextEdit::multiline(&mut text).desired_width(1000.0).desired_rows(25))
+}
+
+impl eframe::epi::App for MyApp {
+    fn setup(&mut self, _ctx: &egui::Context, _frame: &eframe::epi::Frame, _storage: Option<&dyn eframe::epi::Storage>) {
+        let serial_ports = serialport::available_ports().unwrap();
+        self.comports = serial_ports.iter().map(|port| ComPort::COMPORT(port.port_name.clone())).collect();
+        println!("Serial Ports {:?}", self.comports);
+    }
+
+
+    fn update(&mut self, ctx: &egui::Context, _frame: & eframe::epi::Frame) {
+        egui::TopBottomPanel::top("Menu").show(ctx, |ui| {
+            ui.menu_button("Transfer", |ui| {
+                if ui.button("xModem Send").clicked() {
+                    if let Some(path) = rfd::FileDialog::new().pick_file() {
+                        let picked_path = Some(path.display().to_string());
+                        println!("Picked path: {:?}", picked_path);
+                    }
+                }
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.label("COMM:");
+                egui::ComboBox::from_id_source("COMPORT")
+                .selected_text(format!("{:?}", self.selected_comport))
+                .show_ui(ui, |ui| {
+                        for comport in &self.comports {
+                            match comport {
+                                ComPort::COMPORT(port) => ui.selectable_value(&mut self.selected_comport, ComPort::COMPORT(port.to_string()), port.to_string()),
+                                ComPort::None => ui.selectable_value(&mut self.selected_comport, ComPort::None, ""),
+                            };
+                        }
+                    }
+                );
+                if ui.button("Refresh Ports").clicked() {
+                    let serial_ports = serialport::available_ports().unwrap();
+                    self.comports = serial_ports.iter().map(|port| ComPort::COMPORT(port.port_name.clone())).collect();
+                    println!("Serial Ports {:?}", self.comports);
+
+                }
+                ui.label("BAUD:");
+                egui::ComboBox::from_id_source("BAUD")
+                .selected_text(format!("{:?}", self.selected_buadrate))
+                .show_ui(ui, |ui| {
+                        for baudrate in &self.buadrates {
+                            match baudrate {
+                                BuadRates::BAUD(rate) => ui.selectable_value(&mut self.selected_buadrate, BuadRates::BAUD(*rate), rate.to_string()),
+                                BuadRates::None => ui.selectable_value(&mut self.selected_buadrate, BuadRates::None, ""),
+                            };
+                        }
+                    }
+                );
+                if ui.button("Connect").clicked() {
+                    match &self.selected_comport {
+                        ComPort::None => println!("Select a valid Comport!!!!"),
+                        ComPort::COMPORT(port_name) => {
+                            if let Ok(port) = serialport::new(port_name, 9600).timeout(Duration::from_millis(100)).open() {
+                                self.serial_port = Some(port);
+                                println!("Opened the Serial Port!");
+                            }
+                            else {
+                                println!("Can't open port");
+                            }
+                        },
+                    }
+                }
+                if ui.button("Settings").clicked() {
+                    self.serial_settings_flag = !self.serial_settings_flag;
+                }
+                if self.serial_settings_flag {
+                    egui::Window::new("Serial Settings").collapsible(false).open(&mut self.serial_settings_flag).resizable(true).show(ctx, |ui| {
+                        ui.label("Serial Parameters");
+                        ui.horizontal(|ui| {
+                            ui.label("Data:");
+                            egui::ComboBox::from_id_source("BAUD")
+                            .selected_text(format!("{:?}", self.selected_buadrate))
+                            .show_ui(ui, |ui| {
+                                    ui.selectable_value(&mut self.selected_buadrate, BuadRates::None, "");
+                                }
+                            );
+                        });
+                    });
+                }
+            });
+            if ui.button("Get ID").clicked() {
+                let buf = "ID\n\r".as_bytes();
+                match self.serial_port {
+                    Some(_) => self.serial_port.as_mut().unwrap().write(buf).unwrap(),
+                    None => 0,
+                };
+
+                let mut read_buffer: Vec<u8> = vec![0; 1000];
+                match self.serial_port.as_mut().unwrap().read(&mut read_buffer[..]) {
+                    Err(_) => println!("Got an IO Error"),
+                    Ok(bytes) => println!("Bytes read: {bytes}"),
+                }
+                self.console_text = std::str::from_utf8(&read_buffer).unwrap().to_string();
+            }
+
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                if selectable_text(ui, &mut self.console_text).has_focus() {
+                    let events = ui.input().events.clone(); // avoid dead-lock by cloning. TODO: optimize
+                    for event in &events {
+                        match event {
+                            Event::Text(text_to_insert) => {
+                                // Newlines are handled by `Key::Enter`.
+                                if !text_to_insert.is_empty() && text_to_insert != "\n" && text_to_insert != "\r" {
+                                    self.serial_port.as_mut().unwrap().write(text_to_insert.as_bytes()).unwrap();
+                                    let mut read_buffer: Vec<u8> = vec![0; 1000];
+                                    match self.serial_port.as_mut().unwrap().read(&mut read_buffer[..]) {
+                                        Err(err) => println!("Got an IO Error: {err}"),
+                                        Ok(bytes) => println!("Bytes read: {bytes}"),
+                                    }
+                                    self.console_text.push_str(std::str::from_utf8(&read_buffer).unwrap());
+                                } else {
+                                }
+                            }
+                            Event::Key {
+                                key: Key::Enter,
+                                pressed: true,
+                                ..
+                            } => {
+                                self.serial_port.as_mut().unwrap().write("\r\n".as_bytes()).unwrap();
+                                let mut read_buffer: Vec<u8> = vec![0; 1000];
+                                match self.serial_port.as_mut().unwrap().read(&mut read_buffer[..]) {
+                                    Err(err) => println!("Got an IO Error: {err}"),
+                                    Ok(bytes) => println!("Bytes read: {bytes}"),
+                                }
+                                self.console_text.push_str(std::str::from_utf8(&read_buffer).unwrap());
+                            }
+                            _ => (),
+                        };
+                    }
+                }
+            });
+        });
+    }
+
+    fn name(&self) -> &str {
+        self.name.as_str()
+    }
+}
+
+// fn main() {
+//     let builder = serialport::new("COM11", 115200)
+//         .data_bits(DataBits::Eight)
+//         .stop_bits(StopBits::One);
+
+//     let mut port = builder.open().expect("Failed to open port");
+//     port.set_timeout(Duration::new(1, 0)).unwrap();
+
+//     let mut xmodem: XModem = XModem::new(port);
+
+//     let stream = File::open("example.txt").unwrap();
+//     xmodem.send(Box::new(stream)).unwrap();
+
+//     // let stream = File::create("example2.txt").unwrap();
+//     // xmodem.receive(Box::new(stream), true).unwrap();
+
+// }
+
---
+++ src/xmodem.rs
@@ -0,0 +1,371 @@
+
+use serialport::SerialPort;
+use std::io::{Read, Write};
+
+
+pub struct XModem
+{
+    /// Serial device
+    uart: Box<dyn SerialPort>,
+    /// Maximum retries
+    retries: i32,
+    /// Padding bytes
+    padbyte: u8
+}
+
+const SOH: u8 = 0x01;
+const STX: u8 = 0x02;
+const EOT: u8 = 0x04;
+const ACK: u8 = 0x06;
+const NAK: u8 = 0x15;
+const CAN: u8 = 0x18;
+const SUB: u8 = 0x1A;
+const CRC: u8 = 0x43;
+
+
+impl XModem
+{
+    pub fn new(device: Box<dyn SerialPort>) -> Self
+    {
+        Self {
+            uart: device,
+            retries: 16,
+            padbyte: SUB,
+        }
+    }
+
+    /// Set the maximum number of retries
+    pub fn retries(mut self, retries: i32) -> Self {
+        self.retries = retries;
+        self
+    }
+
+    /// Set the padding bytes
+    pub fn padbyte(mut self, padbyte: u8) -> Self {
+        self.padbyte = padbyte;
+        self
+    }
+
+    fn send_nak(&mut self) {
+        let packet: Vec<u8> = vec![NAK];
+        self.uart.as_mut().write(&packet[..]).expect("Failed Send Transmission Byte");
+    }
+
+    fn send_ack(&mut self) {
+        let packet: Vec<u8> = vec![ACK];
+        self.uart.as_mut().write(&packet[..]).expect("Failed Send Transmission Byte");
+    }
+
+    /// Receives to a stream on the XModem protocol
+    pub fn receive(&mut self, mut stream: Box<dyn Write>, crc_mode: bool) -> Result<usize, &'static str> {
+        let mut errors = 0;
+        let mut size = 0;
+        let mut cancel = false;
+        // Synchronization
+        let buf = if crc_mode {vec![CRC]}  else { vec![NAK]};
+        self.uart.as_mut().write(&buf[..]).expect("Sync I/O failure");
+        // Receive Packets
+        let mut data_length: usize = 128;
+        let mut packet_num: u8 = 1;
+        loop {
+
+            let mut header = vec![0; 1];
+            // Read Header
+            match self.uart.as_mut().read(&mut header) {
+                Ok(_) => {
+                    println!("Data received {:?}", header);
+
+                    match header[0] {
+                        SOH => data_length = 128,
+                        STX => data_length = 1024,
+                        EOT => break,
+                        CAN => 
+                        {
+                            if cancel {
+                                return Err("Cancelled got CAN Twice")
+                            }
+                            cancel = true;
+                            continue;
+                        }
+                        _ => {
+                            self.uart.as_mut().write(&buf[..]).expect("Sync I/O failure");
+                            errors += 1;
+                            if errors > self.retries {
+                                return Err("Synchronization failed, reached max number of retries");
+                            }
+                            continue;
+                        }
+                    }
+                }
+                _ => {
+                    errors += 1;
+                    if errors > self.retries {
+                        return Err("Packet Send Failed, reached max number of retries");
+                    }
+                }
+            }
+
+            // Read rest of packet.
+            let packet_length = if crc_mode { data_length + 4 } else { data_length + 3};
+            let mut packet = vec![0; packet_length];
+            match self.uart.as_mut().read(&mut packet) {
+                Ok(_) => {
+                    println!("Data received {:?}", packet);
+
+                    let pn1 = packet[0];
+                    let pn2 = packet[1];
+                    if (pn1 + pn2) != 0xff {
+                        println!("Error Packet Number was not expected");
+                        errors += 1;
+                        self.send_nak();
+                        continue;
+                    }
+                    else if pn1 != packet_num {
+                        println!("Error Packet Number was not expected");
+                        errors += 1;
+                        self.send_nak();
+                        continue;
+                    }
+                    if errors > self.retries {
+                        return Err("Packet Send Failed, reached max number of retries");
+                    }
+
+                    if crc_mode {
+                        let calc_crc = crc(&packet[2..packet_length - 2]);
+                        let received_crc = ((packet[packet_length - 2] as u16) << 8) | packet[packet_length - 1] as u16;
+                        if received_crc != calc_crc
+                        {
+                            println!("CRC error: theirs {received_crc}, ours {calc_crc}");
+                            errors += 1;
+                            self.send_nak();
+                            continue;
+                        }
+                    }
+                    else {
+                        let calc_checksum = checksum(&packet[2..packet_length - 1]);
+                        let received_checksum = packet[packet_length - 1];
+                        if calc_checksum != received_checksum {
+                            println!("Check sum error: theirs {received_checksum}, ours {calc_checksum}");
+                            errors += 1;
+                            self.send_nak();
+                            continue;
+                        }
+                    }
+
+                    size += data_length;
+                    stream.as_mut().write(&packet[2..packet_length - 1]).expect("Failed to write to stream");
+                    println!("Send ACK");
+                    self.send_ack();
+                    packet_num += 1;
+
+                }
+                _ => {
+                    errors += 1;
+                    if errors > self.retries {
+                        return Err("Packet Send Failed, reached max number of retries");
+                    }
+                }
+            }
+        }
+        self.send_ack();
+        println!("Data received, size: {size}");
+        Ok(size)
+    }
+
+    /// Sends a stream over the XModem protocol
+    pub fn send(&mut self, mut stream: Box<dyn Read>) -> Result<(), &'static str> {
+        let mut errors = 0;
+
+        let mut cancel = false;
+        let mut crc_mode = false;
+        // Synchronize with Reciever
+        loop {
+            let mut bytes = [0; 1];
+            match self.uart.as_mut().read(&mut bytes) {
+                Ok(_) => {
+                    let byte = bytes[0];
+                    println!("Receiver Byte: {}, Errors: {}", byte, errors);
+                    match byte {
+                        NAK => break,
+                        CRC => {
+                            println!("Use CRC Mode");
+                            crc_mode = true;
+                            break;
+                        }
+                        CAN => 
+                        {
+                            if cancel {
+                                return Err("Cancelled got CAN Twice")
+                            }
+                            cancel = true;
+                        }
+                        EOT => return Err("Cancelled got EOT"),
+                        _ => {
+                            errors += 1;
+                            if errors > self.retries {
+                                return Err("Synchronization failed, reached max number of retries");
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    errors += 1;
+                    if errors > self.retries {
+                        return Err("Synchronization failed, reached max number of retries");
+                    }
+                }
+            }
+        }
+
+        // Send Packets
+        errors = 0;
+        let packet_length: usize = 128;
+        let mut packet_num: u8 = 1;
+        self.uart.clear(serialport::ClearBuffer::Input).expect("Failed to clear buffer");
+        loop {
+            let mut data: Vec<u8> = vec![self.padbyte; packet_length];
+            match stream.as_mut().read(&mut data) {
+                Ok(0) => break,
+                Ok(len) => {
+                    loop {
+                        // Emit Packet
+                        let mut packet: Vec<u8> = vec![0];
+                        let seq2: u8 = 0xff - packet_num;
+                        println!("PacketNum: {}", packet_num);
+                        println!("PacketNum Inverse: {}", seq2);
+                        println!("Stream Data Len: {}", len);
+                        println!("Data to send {:?}", data);
+                        packet.push(SOH);
+                        packet.push(packet_num);
+                        packet.push(seq2);
+                        for val in &data {
+                            packet.push(val.clone());
+                        }
+
+                        if crc_mode {
+                            let crc = crc(&data);
+                            let hi_crc_byte: u8 = (crc >> 8) as u8;
+                            let lo_crc_byte: u8 = (crc & 0xff) as u8;
+                            println!("CRC: {}", crc);
+                            packet.push(hi_crc_byte);
+                            packet.push(lo_crc_byte);
+                        }
+                        else {
+                            let checksum = checksum(&data);
+                            println!("Checksum: {}", checksum);
+                            packet.push(checksum);
+                        }
+                        self.uart.as_mut().write(&packet[..]).expect("Failed to Send Bytes");
+                        self.uart.clear(serialport::ClearBuffer::Input).expect("Failed to clear buffer");
+                        assert!(self.uart.bytes_to_read().unwrap() == 0);
+                        let mut bytes = [0; 1];
+                        // Get Receiver ACK
+                        match self.uart.as_mut().read_exact(&mut bytes) {
+                            Ok(_) => {
+                                println!("Data received {:?}", bytes);
+                                let byte = bytes[0];
+                                println!("Receiver Byte: {}, Errors: {}", byte, errors);
+                                match byte {
+                                    ACK => {
+                                        if packet_num == 255 {
+                                            packet_num = 0;
+                                        }
+                                        packet_num += 1;
+                                        break;
+                                    }
+                                    NAK => {
+                                        errors += 1;
+                                        println!("Received NAK resending");
+                                        if errors > self.retries {
+                                            return Err("Packet Send Failed, reached max number of retries");
+                                        }
+                                    }
+                                    _ => {
+                                        errors += 1;
+                                        if errors > self.retries {
+                                            return Err("Packet Send Failed, reached max number of retries");
+                                        }
+                                    }
+                                }
+                            }
+                            _ => {
+                                errors += 1;
+                                if errors > self.retries {
+                                    return Err("Packet Send Failed, reached max number of retries");
+                                }
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    return Err("IO Read Error");
+                }
+            } 
+        }
+
+        // End of Transmission Sync
+        loop {
+            let packet: Vec<u8> = vec![EOT];
+            self.uart.clear(serialport::ClearBuffer::Input).expect("Failed to clear buffer");
+            assert!(self.uart.bytes_to_read().unwrap() == 0);
+            self.uart.as_mut().write(&packet[..]).expect("Failed Send Transmission Byte");
+            let mut bytes = [0; 1];
+            match  self.uart.as_mut().read_exact(&mut bytes) {
+                Ok(_) => {
+                    let byte = bytes[0];
+                    println!("End Sync Received Byte: {}, Errors: {}", byte, errors);
+                    match byte {
+                        ACK => break,
+                        _ => {
+                            errors += 1;
+                            if errors > self.retries {
+                                return Err("End of Transmission Sync, reached max number of retries");
+                            }
+                        }
+                    }
+                }
+                _ => return Err("End of Transmission Sync I/O failure"),
+            }
+        }
+        Ok(())
+    }
+
+
+
+
+
+}
+
+/// Calculates 8bit XModem checksum
+fn checksum(data: &[u8]) -> u8 {
+    let sum: u32 = data.iter().map(|&val| val as u32).sum();
+    let checksum = (sum % 256) as u8;
+    return checksum; 
+}
+
+/// Calculate 16bit XModem CRC
+fn crc(data: &[u8]) -> u16 {
+    let mut crc = 0;
+    for val in data {
+        let item: i32 = val.clone().into();
+        crc = crc ^ (item << 8);
+        for _ in 0..8 {
+            crc = crc << 1;
+            if crc & 0x10000 > 0 {
+                crc = (crc ^ 0x1021) & 0xffff;
+            }
+        }
+    }
+    return crc as u16; 
+}
+
+
+#[cfg(test)]
+
+#[test]
+fn test_crc()
+{
+    let data: Vec<u8> = vec![0x12, 0x34, 0x56, 0x78, 0x09];
+    let result = crc(&data);
+    assert_eq!(result, 0x5A76);
+}
